883d55cb2fd8d163aae8727dc8828dfaacbf49d6
bannakonz <bannakon@gmail.com>
2023-10-01 16:41:59 +0700

    add button form

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -72,6 +72,26 @@ class _NewItemState extends State<NewItem> {
                     ),
                   ),
                 ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Reset',
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Add Item',
+                    ),
+                  ),
+                ],
               )
             ],
           ),
