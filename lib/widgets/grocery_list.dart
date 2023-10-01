6e2fa94a6244ee452f024184f84c6dc1efb0d968
bannakonz <bannakon@gmail.com>
2023-10-01 09:34:32 +0700

    add new item screen

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +28,14 @@ class GroceryList extends StatelessWidget {
             'Your Grocery',
           ),
         ),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(
+              Icons.add,
+            ),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
@@ -24,7 +46,9 @@ class GroceryList extends StatelessWidget {
             height: 24,
             color: groceryItems[index].category.color,
           ),
-          trailing: Text(groceryItems[index].quantity.toString(),),
+          trailing: Text(
+            groceryItems[index].quantity.toString(),
+          ),
         ),
       ),
     );
