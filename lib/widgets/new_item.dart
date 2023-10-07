7768e82c2aee81822770a0856eb0bee9d5653fc5
bannakonz <bannakon@gmail.com>
2023-10-07 16:32:35 +0700

    await response

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -21,23 +21,27 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  Future<void> _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https('fluter-prep-3c6a7-default-rtdb.firebaseio.com', 'shopping-list.json');
-      http.post(url, headers: {
+      final response = await http.post(url, headers: {
         'Content-Type': 'application/json',
       }, body: json.encode({
         'name': _enteredName,
         'quantity': _enteredQuantity,
         'category': _selectedCategory.title,
       }));
-      // Navigator.of(context).pop(GroceryItem(
-      //   id: DateTime.now().toString(),
-      //   name: _enteredName,
-      //   quantity: _enteredQuantity,
-      //   category: _selectedCategory,
-      // ));
+
+      print(response.statusCode);
+      print(response.body);
+
+      if(!context.mounted) {
+        print('mount');
+        return;
+      }
+
+      Navigator.of(context).pop();
     }
   }
 
