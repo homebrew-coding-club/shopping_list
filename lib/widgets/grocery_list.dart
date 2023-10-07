51e614e676492f8a12766679a2947afd29dc1597
bannakonz <bannakon@gmail.com>
2023-10-07 21:54:47 +0700

    fetch data

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shopping_list/data/categories.dart';
+
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
@@ -10,7 +15,36 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItem();
+  }
+
+  void _loadItem() async {
+    final url = Uri.https(
+        'fluter-prep-3c6a7-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries.firstWhere((catItem) => catItem.value.title == item.value['category']).value;
+      loadedItems.add(
+          GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category,
+          )
+      );
+    }
+    setState(() {
+      _groceryItems = loadedItems;
+    });
+  }
+
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
@@ -19,7 +53,7 @@ class _GroceryListState extends State<GroceryList> {
       ),
     );
 
-    if(newItem == null) {
+    if (newItem == null) {
       return;
     }
     setState(() {
@@ -36,10 +70,12 @@ class _GroceryListState extends State<GroceryList> {
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(
-      child: Text('No item data yet',),
+      child: Text(
+        'No item data yet',
+      ),
     );
 
-    if(_groceryItems.isNotEmpty) {
+    if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
         itemCount: _groceryItems.length,
         itemBuilder: (ctx, index) => Dismissible(
